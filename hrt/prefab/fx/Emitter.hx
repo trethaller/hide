73ae826bedb0fbb46a446d4599b96d23c8fe1d44
trethaller <tom.rethaller@gmail.com>
2026-03-09 16:21:16 +0100

    remove prefetch

--- hrt/prefab/fx/Emitter.hx
+++ hrt/prefab/fx/Emitter.hx
@@ -1496,7 +1496,7 @@ class EmitterObject extends h3d.scene.Object {
 		if (scene == null)
 			return;
 
-		evaluator.prefetch();
+		// evaluator.prefetch();
 
 		switch(simulationSpace){
 			// Particles in Local are spawned next to emitter in the scene tree,
